91ccbcd0e0fc459acdf1f9ea2e29cdc8c4bcf912
Rodrigo.Damico <damico.rodrigo@hotmail.com>
2019-06-27 14:20:40 -0300

    Inicio de proyecto web

---
+++ usherWeb/src/main/java/team101/usher/App.java
@@ -0,0 +1,13 @@
+package team101.usher;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hola Usher Web!!!!" );
+        
+    }
+}
